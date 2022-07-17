28ecd321362bcae8b7edab6d8254d372f8f2f98d
jesuspurizaga <jesus.purizaga@lundero.com>
2022-07-17 12:05:22 -0500

    cods and duplicated

---
+++ codigos.py
@@ -0,0 +1,31 @@
+import pyodbc
+from sqlalchemy import create_engine
+import pandas as pd
+
+try:
+    engine = create_engine("mssql+pyodbc://sa:kali21@DESKTOP-KLRPEIV/dbtortelin?driver=ODBC+Driver+17+for+SQL+Server")
+    print("conexión establecida")
+except Exception as ex:
+    print(ex)
+
+codigos = pd.read_sql_query("SELECT CONCAT(SUBSTRING(sabor,1,3),SUBSTRING(REVERSE(sabor),1,3),SUBSTRING(tamanio,1,3)) as codigo, sabor, tamanio from (select distinct sabor, tamanio from tabla_provisional) as fr;", engine)
+codigos = codigos.values.tolist()
+
+server = 'DESKTOP-KLRPEIV'
+bd = 'dbtortelin'
+usuario = 'sa'
+contrasena = 'kali21'
+
+
+try:
+    conexion = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL server}; SERVER='+server+'; DATABASE='+bd+'; UID='+usuario+';PWD='+contrasena)
+    cur = conexion.cursor()
+    print("conexion exitosa")
+except Exception as ex:
+    print(ex)
+
+for codigo, sabor, tamaño in codigos:
+    print(codigo, sabor, tamaño)
+    cur.execute("UPDATE registro_tortelin SET codigo = ? WHERE sabor = ? and tamanio = ?;", codigo, sabor, tamaño)
+    conexion.commit()
+
---
+++ eliminar_duplicados.py
@@ -0,0 +1,40 @@
+import pyodbc
+from sqlalchemy import create_engine
+import pandas as pd
+
+server = 'DESKTOP-KLRPEIV'
+bd = 'dbtortelin'
+usuario = 'sa'
+contrasena = 'kali21'
+
+try:
+    conexion = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL server}; SERVER='+server+'; DATABASE='+bd+'; UID='+usuario+';PWD='+contrasena)
+    cur = conexion.cursor()
+    print("conexion exitosa")
+except Exception as ex:
+    print(ex)
+
+cur.execute("select fecha, nombre_local, tamanio, sabor from registro_tortelin group by fecha, nombre_local, tamanio, sabor having count(*) > 1")
+duplicados = cur.fetchall()
+print(duplicados)
+
+for fecha, local, tamaño, sabor in duplicados:
+    print(fecha, local, tamaño, sabor)
+    cur.execute("select unidades_vendidas, unidades_desechadas, precio_dia from registro_tortelin where nombre_local= ? and tamanio = ? and sabor = ? and fecha = ?", local, tamaño, sabor, fecha)
+    duplicado = cur.fetchall()
+    print(duplicado)
+    univend = duplicado[0][0] + duplicado[1][0]
+    unidesech = duplicado[0][1] + duplicado[1][1]
+    if duplicado[0][0] == duplicado[0][1] and duplicado[0][1] == duplicado[1][1] and duplicado[0][2] == duplicado[1][2]:
+        print("completamente iguales")
+    else:
+        if duplicado[0][2] > duplicado[1][2]:
+            precio = duplicado[0][2]
+            cur.execute("update registro_tortelin set unidades_vendidas = ?, unidades_desechadas = ? where nombre_local=? and tamanio =? and sabor = ? and fecha = ? and precio_dia = ?;", univend, unidesech, local, tamaño, sabor, fecha, precio)
+            cur.execute("delete from registro_tortelin where unidades_vendidas = ? and unidades_desechadas = ? and nombre_local=? and tamanio =? and sabor = ? and fecha = ? and precio_dia = ?;", duplicado[1][0], duplicado[1][1], local, tamaño, sabor, fecha, duplicado[1][2])
+            conexion.commit()
+        else:
+            precio = duplicado[1][2]
+            cur.execute("update registro_tortelin set unidades_vendidas = ?, unidades_desechadas = ? where nombre_local=? and tamanio =? and sabor = ? and fecha = ? and precio_dia = ?;", univend, unidesech, local, tamaño, sabor, fecha, precio)
+            cur.execute("delete from registro_tortelin where unidades_vendidas = ? and unidades_desechadas = ? and nombre_local=? and tamanio =? and sabor = ? and fecha = ? and precio_dia = ?;", duplicado[0][0], duplicado[0][1], local, tamaño, sabor, fecha, duplicado[0][2])
+            conexion.commit() 
